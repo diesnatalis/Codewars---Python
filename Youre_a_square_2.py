a1d0bd756c110ac1265bc43418c792a64c298156
diesnatalis <natalia.grams777@gmail.com>
2017-11-21 18:54:23 +0100

    Update Youre_a_square_2.py

--- Youre_a_square_2.py
+++ Youre_a_square_2.py
@@ -1,2 +1,2 @@
 def is_square(n):
-  return n>0 and (n**0.5).is_integer()
+  return n>0 and (n**0.5).is_integer() # is_integer() return false or true. We can write "print(n.is_integer())" and show false or true. Number n must be float.
