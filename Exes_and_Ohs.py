f90ad66cfe76aff7a5412835f49c7718b75f3f3a
diesnatalis <natalia.grams777@gmail.com>
2017-11-21 20:39:03 +0100

    Update and rename Exes_and_Ohs to Exes_and_Ohs.py

--- Exes_and_Ohs.py
+++ Exes_and_Ohs.py
@@ -1,6 +1,8 @@
 def xo(s):
     if s.count("x")+s.count("X")==s.count("o")+s.count("O"):
         return True
+    else:
+        return False
         
         
 # Python’s boolean constants are capitalized: "True" and "False".
